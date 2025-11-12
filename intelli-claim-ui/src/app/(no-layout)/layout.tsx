ff50d4e903c6153d9cf4e2b2b4ce1dfa74f9f231
Pranav <masterpranav07@gmail.com>
2025-11-12 16:40:29 +0530

    feat: Create RootLayout component for application structure

---
+++ intelli-claim-ui/src/app/(no-layout)/layout.tsx
@@ -0,0 +1,16 @@
+import "@/app/globals.css";
+import "./../../../node_modules/@hdfclife-insurance/one-x-ui/styles.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body >
+        {children}
+      </body>
+    </html>
+  );
+}
